3661a86a8b4a51c296c3ef1bb921ab7ee6b7feba
Your Name <you@example.com>
2022-03-19 13:06:34 -0300

    new try

--- list.c
+++ list.c
@@ -143,22 +143,14 @@ void * popCurrent(List * list)
     Node *datoEliminado = list->current->data;
 
     if (list->current->prev != NULL)
-    {
         list->current->prev->next = list->current->next;
-    }
     else
-    {
         list->head = list->current->next;
-    }
 
     if (list->current->next != NULL)
-    {
         list->current->next->prev = list->current->prev;
-    }
     else
-    {
         list->tail = list->current->prev;
-    }
 
     return datoEliminado;
 }
